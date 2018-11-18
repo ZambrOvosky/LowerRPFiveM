d802d02c432e75abffd5a3e6714c291193c69105
Victor <jvdelicia123@outlook.com>
2018-11-18 20:45:04 -0200

    First commit + Server

---
+++ resources/vrp_showroom2/client.lua
@@ -0,0 +1,511 @@
+--[[
+    FiveM Scripts
+    Copyright C 2018  Sighmir
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    at your option any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+
+vRP = Proxy.getInterface("vRP")
+vRPg = Proxy.getInterface("vRP_garages")
+
+function deleteVehiclePedIsIn()
+  local v = GetVehiclePedIsIn(GetPlayerPed(-1),false)
+  SetVehicleHasBeenOwnedByPlayer(v,false)
+  Citizen.InvokeNative(0xAD738C3085FE7E11, v, false, true) -- set not as mission entity
+  SetVehicleAsNoLongerNeeded(Citizen.PointerValueIntInitialized(v))
+  Citizen.InvokeNative(0xEA386986E786A54F, Citizen.PointerValueIntInitialized(v))
+end
+
+local vehshop = {
+	opened = false,
+	title = "TeaBag Shops",
+	currentmenu = "main",
+	lastmenu = nil,
+	currentpos = nil,
+	selectedbutton = 0,
+	marker = { r = 0, g = 155, b = 255, a = 200, type = 1 },
+	menu = {
+		x = 0.1,
+		y = 0.08,
+		width = 0.2,
+		height = 0.04,
+		buttons = 10,
+		from = 1,
+		to = 10,
+		scale = 0.4,
+		font = 0,
+		["main"] = {
+			title = "TeaBag Shops",
+			name = "main",
+			buttons = {
+				{name = "Carros", description = ""},
+			}
+		},
+		["Carros"] = {
+			title = "Carros",
+			name = "Carros",
+			buttons = {
+			    {name = "Edicoes Especiais", description = ''},
+			}
+		},
+		["Edicoes Especiais"] = {
+			title = "Edicoes Especiais",
+			name = "Edicoes Especiais",
+			buttons = {
+				{name = "Passat", costs = 300000, description = {}, model = "passat"},
+				{name = "Blazer", costs = 200000, description = {}, model = "baller"},
+				{name = "Fiat Punto", costs = 400000, description = {}, model = "dilettante"},
+				{name = "Gol G3", costs = 150000, description = {}, model = "asea"},
+				{name = "Gol G5", costs = 180000, description = {}, model = "blista"},
+				{name = "Nissan Frontier", costs = 300000, description = {}, model = "sadler"},
+			}
+		},
+	}
+}
+local fakecar = {model = '', car = nil}
+local vehshop_locations = {
+{entering = {311.48199462891,-701.74200439453,29.968023300171}, inside = {300.63146972656,-693.41485595703,29.969511032104}, outside = {322.86572265625,-701.67840576172,29.259679794312}},
+}
+
+local vehshop_blips ={}
+local inrangeofvehshop = false
+local currentlocation = nil
+local boughtcar = false
+
+function vehSR_drawTxt(text,font,centre,x,y,scale,r,g,b,a)
+	SetTextFont(font)
+	SetTextProportional(0)
+	SetTextScale(scale, scale)
+	SetTextColour(r, g, b, a)
+	SetTextDropShadow(0, 0, 0, 0,255)
+	SetTextEdge(1, 0, 0, 0, 255)
+	SetTextDropShadow()
+	SetTextOutline()
+	SetTextCentre(centre)
+	SetTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawText(x , y)
+end
+
+function vehSR_IsPlayerInRangeOfVehshop()
+	return inrangeofvehshop
+end
+
+local firstspawn = 0
+AddEventHandler('playerSpawned', function(spawn)
+if firstspawn == 0 then
+	--326 car blip 227 225
+	vehSR_ShowVehshopBlips(true)
+	firstspawn = 1
+end
+end)
+
+function vehSR_ShowVehshopBlips(bool)
+	if bool and #vehshop_blips == 0 then
+		for station,pos in pairs(vehshop_locations) do
+			local loc = pos
+			pos = pos.entering
+			local blip = AddBlipForCoord(pos[1],pos[2],pos[3])
+			-- 60 58 137
+			SetBlipSprite(blip,225)
+			BeginTextCommandSetBlipName("STRING")
+			AddTextComponentString("Apollo Shops")
+			EndTextCommandSetBlipName(blip)
+			SetBlipAsShortRange(blip,true)
+			SetBlipAsMissionCreatorBlip(blip,true)
+			table.insert(vehshop_blips, {blip = blip, pos = loc})
+		end
+		Citizen.CreateThread(function()
+			while #vehshop_blips > 0 do
+				Citizen.Wait(0)
+				local inrange = false
+				for i,b in ipairs(vehshop_blips) do
+					if IsPlayerWantedLevelGreater(GetPlayerIndex(),0) == false and vehshop.opened == false and IsPedInAnyVehicle(vehSR_LocalPed(), true) == false and  GetDistanceBetweenCoords(b.pos.entering[1],b.pos.entering[2],b.pos.entering[3],GetEntityCoords(vehSR_LocalPed())) < 5 then
+						DrawMarker(1,b.pos.entering[1],b.pos.entering[2],b.pos.entering[3],0,0,0,0,0,0,2.001,2.0001,0.5001,0,155,255,200,0,0,0,0)
+						vehSR_drawTxt("Aperte ~g~ENTER~s~ para comprar um ~b~Carro",0,1,0.5,0.8,0.6,255,255,255,255)
+						currentlocation = b
+						inrange = true
+					end
+				end
+				inrangeofvehshop = inrange
+			end
+		end)
+	elseif bool == false and #vehshop_blips > 0 then
+		for i,b in ipairs(vehshop_blips) do
+			if DoesBlipExist(b.blip) then
+				SetBlipAsMissionCreatorBlip(b.blip,false)
+				Citizen.InvokeNative(0x86A652570E5F25DD, Citizen.PointerValueIntInitialized(b.blip))
+			end
+		end
+		vehshop_blips = {}
+	end
+end
+
+function vehSR_f(n)
+	return n + 0.0001
+end
+
+function vehSR_LocalPed()
+	return GetPlayerPed(-1)
+end
+
+function vehSR_try(f, catch_f)
+	local status, exception = pcall(f)
+	if not status then
+		catch_f(exception)
+	end
+end
+function vehSR_firstToUpper(str)
+    return (str:gsub("^%l", string.upper))
+end
+--local veh = nil
+function vehSR_OpenCreator()
+	boughtcar = false
+	local ped = vehSR_LocalPed()
+	local pos = currentlocation.pos.inside
+	FreezeEntityPosition(ped,true)
+	SetEntityVisible(ped,false)
+	local g = Citizen.InvokeNative(0xC906A7DAB05C8D2B,pos[1],pos[2],pos[3],Citizen.PointerValueFloat(),0)
+	SetEntityCoords(ped,pos[1],pos[2],g)
+	SetEntityHeading(ped,pos[4])
+	vehshop.currentmenu = "main"
+	vehshop.opened = true
+	vehshop.selectedbutton = 0
+end
+local vehicle_price = 0
+function vehSR_CloseCreator(vehicle,veh_type)
+	Citizen.CreateThread(function()
+		local ped = vehSR_LocalPed()
+		if not boughtcar then
+			local pos = currentlocation.pos.entering
+			SetEntityCoords(ped,pos[1],pos[2],pos[3])
+			FreezeEntityPosition(ped,false)
+			SetEntityVisible(ped,true)
+		else
+			deleteVehiclePedIsIn()
+			vRP.teleport({322.86572265625,-701.67840576172,29.259679794312})
+			vRPg.spawnBoughtVehicle({veh_type, vehicle})
+			SetEntityVisible(ped,true)
+			FreezeEntityPosition(ped,false)
+		end
+		vehshop.opened = false
+		vehshop.menu.from = 1
+		vehshop.menu.to = 10
+	end)
+end
+
+function vehSR_drawMenuButton(button,x,y,selected)
+	local menu = vehshop.menu
+	SetTextFont(menu.font)
+	SetTextProportional(0)
+	SetTextScale(menu.scale, menu.scale)
+	if selected then
+		SetTextColour(0, 0, 0, 255)
+	else
+		SetTextColour(255, 255, 255, 255)
+	end
+	SetTextCentre(0)
+	SetTextEntry("STRING")
+	AddTextComponentString(button.name)
+	if selected then
+		DrawRect(x,y,menu.width,menu.height,255,255,255,255)
+	else
+		DrawRect(x,y,menu.width,menu.height,0,0,0,150)
+	end
+	DrawText(x - menu.width/2 + 0.005, y - menu.height/2 + 0.0028)
+end
+
+function vehSR_drawMenuTitle(txt,x,y)
+local menu = vehshop.menu
+	SetTextFont(2)
+	SetTextProportional(0)
+	SetTextScale(0.5, 0.5)
+	SetTextColour(255, 255, 255, 255)
+	SetTextEntry("STRING")
+	AddTextComponentString(txt)
+	DrawRect(x,y,menu.width,menu.height,0,0,0,150)
+	DrawText(x - menu.width/2 + 0.005, y - menu.height/2 + 0.0028)
+end
+function vehSR_tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+function vehSR_Notify(text)
+SetNotificationTextEntry('STRING')
+AddTextComponentString(text)
+DrawNotification(false, false)
+end
+
+function vehSR_drawMenuRight(txt,x,y,selected)
+	local menu = vehshop.menu
+	SetTextFont(menu.font)
+	SetTextProportional(0)
+	SetTextScale(menu.scale, menu.scale)
+	if selected then
+		SetTextColour(0, 0, 0, 255)
+	else
+		SetTextColour(255, 255, 255, 255)
+	end
+	SetTextCentre(0)
+	SetTextEntry("STRING")
+	AddTextComponentString(txt)
+	DrawText(x + menu.width/2 - 0.06, y - menu.height/2 + 0.0028)
+end
+local backlock = false
+Citizen.CreateThread(function()
+	local last_dir
+	while true do
+		Citizen.Wait(0)
+		if IsControlJustPressed(1,201) and vehSR_IsPlayerInRangeOfVehshop() then
+			if vehshop.opened then
+				vehSR_CloseCreator("","")
+			else
+				vehSR_OpenCreator()
+			end
+		end
+		if vehshop.opened then
+			local ped = vehSR_LocalPed()
+			local menu = vehshop.menu[vehshop.currentmenu]
+			vehSR_drawTxt(vehshop.title,1,1,vehshop.menu.x,vehshop.menu.y,1.0, 255,255,255,255)
+			vehSR_drawMenuTitle(menu.title, vehshop.menu.x,vehshop.menu.y + 0.08)
+			vehSR_drawTxt(vehshop.selectedbutton.."/"..vehSR_tablelength(menu.buttons),0,0,vehshop.menu.x + vehshop.menu.width/2 - 0.0385,vehshop.menu.y + 0.067,0.4, 255,255,255,255)
+			local y = vehshop.menu.y + 0.12
+			buttoncount = vehSR_tablelength(menu.buttons)
+			local selected = false
+
+			for i,button in pairs(menu.buttons) do
+				if i >= vehshop.menu.from and i <= vehshop.menu.to then
+
+					if i == vehshop.selectedbutton then
+						selected = true
+					else
+						selected = false
+					end
+					vehSR_drawMenuButton(button,vehshop.menu.x,y,selected)
+					if button.costs ~= nil then
+						if vehshop.currentmenu == "Compactos" or vehshop.currentmenu == "Exemplares" or vehshop.currentmenu == "Sedans" or vehshop.currentmenu == "Esportes" or vehshop.currentmenu == "Esportes Clássicos" or vehshop.currentmenu == "Supers" or vehshop.currentmenu == "Grandes" or vehshop.currentmenu == "Estrada" or vehshop.currentmenu == "SUVS" or vehshop.currentmenu == "Vans" or vehshop.currentmenu == "Edicoes Especiais" or vehshop.currentmenu == "Industrials" or vehshop.currentmenu == "Moto" or vehshop.currentmenu == "Motos" then
+							vehSR_drawMenuRight(button.costs.."$",vehshop.menu.x,y,selected)
+						else
+							vehSR_drawMenuButton(button,vehshop.menu.x,y,selected)
+						end
+					end
+					y = y + 0.04
+					if vehshop.currentmenu == "Compactos" or vehshop.currentmenu == "Exemplares" or vehshop.currentmenu == "Sedans" or vehshop.currentmenu == "Esportes" or vehshop.currentmenu == "Esportes Clássicos" or vehshop.currentmenu == "Supers" or vehshop.currentmenu == "Grandes" or vehshop.currentmenu == "Estrada" or vehshop.currentmenu == "SUVS" or vehshop.currentmenu == "Vans" or vehshop.currentmenu == "Edicoes Especiais" or vehshop.currentmenu == "Industrials" or vehshop.currentmenu == "Moto" or vehshop.currentmenu == "Motos" then
+						if selected then
+							if fakecar.model ~= button.model then
+								if DoesEntityExist(fakecar.car) then
+									Citizen.InvokeNative(0xEA386986E786A54F, Citizen.PointerValueIntInitialized(fakecar.car))
+								end
+								local pos = currentlocation.pos.inside
+								local hash = GetHashKey(button.model)
+								RequestModel(hash)
+								local timer = 0
+								while not HasModelLoaded(hash) and timer < 255 do
+									Citizen.Wait(1)
+									vehSR_drawTxt("Carregando...",0,1,0.5,0.5,1.5,255,255-timer,255-timer,255)
+									RequestModel(hash)
+									timer = timer + 1
+								end
+								if timer < 255 then
+									local veh = CreateVehicle(hash,pos[1],pos[2],pos[3],pos[4],false,false)
+									while not DoesEntityExist(veh) do
+										Citizen.Wait(1)
+										vehSR_drawTxt("Carregando...",0,1,0.5,0.5,1.5,255,255-timer,255-timer,255)
+									end
+									FreezeEntityPosition(veh,true)
+									SetEntityInvincible(veh,true)
+									SetVehicleDoorsLocked(veh,4)
+									--SetEntityCollision(veh,false,false)
+									TaskWarpPedIntoVehicle(vehSR_LocalPed(),veh,-1)
+									for i = 0,24 do
+										SetVehicleModKit(veh,0)
+										RemoveVehicleMod(veh,i)
+									end
+									fakecar = { model = button.model, car = veh}
+								else
+									timer = 0
+									while timer < 50 do
+										Citizen.Wait(1)
+										vehSR_drawTxt("Falhou!",0,1,0.5,0.5,1.5,255,0,0,255)
+										timer = timer + 1
+									end
+									if last_dir then
+										if vehshop.selectedbutton < buttoncount then
+											vehshop.selectedbutton = vehshop.selectedbutton +1
+											if buttoncount > 10 and vehshop.selectedbutton > vehshop.menu.to then
+												vehshop.menu.to = vehshop.menu.to + 1
+												vehshop.menu.from = vehshop.menu.from + 1
+											end
+										else
+											last_dir = false
+											vehshop.selectedbutton = vehshop.selectedbutton -1
+											if buttoncount > 10 and vehshop.selectedbutton < vehshop.menu.from then
+												vehshop.menu.from = vehshop.menu.from -1
+												vehshop.menu.to = vehshop.menu.to - 1
+											end
+										end
+									else
+										if vehshop.selectedbutton > 1 then
+											vehshop.selectedbutton = vehshop.selectedbutton -1
+											if buttoncount > 10 and vehshop.selectedbutton < vehshop.menu.from then
+												vehshop.menu.from = vehshop.menu.from -1
+												vehshop.menu.to = vehshop.menu.to - 1
+											end
+										else
+											last_dir = true
+											vehshop.selectedbutton = vehshop.selectedbutton +1
+											if buttoncount > 10 and vehshop.selectedbutton > vehshop.menu.to then
+												vehshop.menu.to = vehshop.menu.to + 1
+												vehshop.menu.from = vehshop.menu.from + 1
+											end
+										end
+									end
+								end
+							end
+						end
+					end
+					if selected and IsControlJustPressed(1,201) then
+						vehSR_ButtonSelected(button)
+					end
+				end
+			end
+			if IsControlJustPressed(1,202) then
+				vehSR_Back()
+			end
+			if IsControlJustReleased(1,202) then
+				backlock = false
+			end
+			if IsControlJustPressed(1,188) then
+				last_dir = false
+				if vehshop.selectedbutton > 1 then
+					vehshop.selectedbutton = vehshop.selectedbutton -1
+					if buttoncount > 10 and vehshop.selectedbutton < vehshop.menu.from then
+						vehshop.menu.from = vehshop.menu.from -1
+						vehshop.menu.to = vehshop.menu.to - 1
+					end
+				end
+			end
+			if IsControlJustPressed(1,187)then
+				last_dir = true
+				if vehshop.selectedbutton < buttoncount then
+					vehshop.selectedbutton = vehshop.selectedbutton +1
+					if buttoncount > 10 and vehshop.selectedbutton > vehshop.menu.to then
+						vehshop.menu.to = vehshop.menu.to + 1
+						vehshop.menu.from = vehshop.menu.from + 1
+					end
+				end
+			end
+		end
+
+	end
+end)
+
+
+function vehSR_round(num, idp)
+  if idp and idp>0 then
+    local mult = 10^idp
+    return math.floor(num * mult + 0.5) / mult
+  end
+  return math.floor(num + 0.5)
+end
+function vehSR_ButtonSelected(button)
+	local ped = GetPlayerPed(-1)
+	local this = vehshop.currentmenu
+	local btn = button.name
+	if this == "main" then
+		if btn == "Carros" then
+			vehSR_OpenMenu('Carros')
+		elseif btn == "Motos" then
+			vehSR_OpenMenu('Motos')
+		end
+	elseif this == "Carros" then
+		if btn == "Esportes" then
+			vehSR_OpenMenu('Esportes')
+		elseif btn == "Sedans" then
+			vehSR_OpenMenu('Sedans')
+		elseif btn == "Compactos" then
+			vehSR_OpenMenu('Compactos')
+		elseif btn == "Exemplares" then
+			vehSR_OpenMenu('Exemplares')
+		elseif btn == "Esportes Clássicos" then
+			vehSR_OpenMenu("Esportes Clássicos")
+		elseif btn == "Supers" then
+			vehSR_OpenMenu('Supers')
+		elseif btn == "Grandes" then
+			vehSR_OpenMenu('Grandes')
+		elseif btn == "Estrada" then
+			vehSR_OpenMenu('Estrada')
+		elseif btn == "SUVS" then
+			vehSR_OpenMenu('SUVS')
+		elseif btn == "Vans" then
+			vehSR_OpenMenu('Vans')
+			elseif btn == "Edicoes Especiais" then
+			vehSR_OpenMenu('Edicoes Especiais')
+		end
+	elseif this == "Compactos" or this == "Exemplares" or this == "Sedans" or this == "Esportes" or this == "Esportes Clássicos" or this == "Supers" or this == "Grandes" or this == "Estrada" or this == "SUVS" or this == "Vans" or this == "Edicoes Especiais" or this == "Industrial" then
+		TriggerServerEvent('veh_SR:CheckMoneyForVeh',button.model,button.costs, "car")
+    elseif this == "Moto" or this == "Motos" then
+		TriggerServerEvent('veh_SR:CheckMoneyForVeh',button.model,button.costs, "bike")
+	end
+end
+
+RegisterNetEvent('veh_SR:CloseMenu')
+AddEventHandler('veh_SR:CloseMenu', function(vehicle, veh_type)
+	boughtcar = true
+	vehSR_CloseCreator(vehicle,veh_type)
+end)
+
+function vehSR_OpenMenu(menu)
+	fakecar = {model = '', car = nil}
+	vehshop.lastmenu = vehshop.currentmenu
+	if menu == "Carros" then
+		vehshop.lastmenu = "main"
+	elseif menu == "bikes"  then
+		vehshop.lastmenu = "main"
+	elseif menu == 'race_create_objects' then
+		vehshop.lastmenu = "main"
+	elseif menu == "race_create_objects_spawn" then
+		vehshop.lastmenu = "race_create_objects"
+	end
+	vehshop.menu.from = 1
+	vehshop.menu.to = 10
+	vehshop.selectedbutton = 0
+	vehshop.currentmenu = menu
+end
+
+
+function vehSR_Back()
+	if backlock then
+		return
+	end
+	backlock = true
+	if vehshop.currentmenu == "main" then
+		vehSR_CloseCreator("","")
+	elseif vehshop.currentmenu == "Compactos" or vehshop.currentmenu == "Exemplares" or vehshop.currentmenu == "Sedans" or vehshop.currentmenu == "Esportes" or vehshop.currentmenu == "Esportes Clássicos" or vehshop.currentmenu == "Supers" or vehshop.currentmenu == "Grandes" or vehshop.currentmenu == "Estrada" or vehshop.currentmenu == "SUVS" or vehshop.currentmenu == "Vans" or vehshop.currentmenu == "Edicoes Especiais" or vehshop.currentmenu == "Industrial" or vehshop.currentmenu == "Moto" or vehshop.currentmenu == "Motos" then
+		if DoesEntityExist(fakecar.car) then
+			Citizen.InvokeNative(0xEA386986E786A54F, Citizen.PointerValueIntInitialized(fakecar.car))
+		end
+		fakecar = {model = '', car = nil}
+		vehSR_OpenMenu(vehshop.lastmenu)
+	else
+		vehSR_OpenMenu(vehshop.lastmenu)
+	end
+
+end
+
+function vehSR_stringstarts(String,Start)
+   return string.sub(String,1,string.len(Start))==Start
+end
